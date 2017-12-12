9dfd35297e53ad1db568addd7fc6f9293aee38b2
alicewy <alicewy@hawaii.edu>
2017-12-11 23:30:38 -1000

    got geolocation working on map-page

--- app/imports/ui/pages/user/index.js
+++ app/imports/ui/pages/user/index.js
@@ -1,6 +1,7 @@
 import './profile-page.html';
 import './profile-page';
 import './map-page';
+import './map-page.css';
 import './map-page.html';
 import './contestWinner.css';
 import './contestWinner-page.html';
